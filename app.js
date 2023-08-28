9997fffde658bcb85014f58caa128e08d0318b58
tanishqacodes <tanishqasharma797@gmail.com>
2023-08-28 21:23:52 +0530

    ticket generation

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { log } = require("util");
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const URI = "mongodb://localhost:27017/ticket";
+const URI = "mongodb://0.0.0.0:27017/ticket";
 
 mongoose.connect(URI).then(()=>{
     console.log("Database connected ");
@@ -29,11 +29,33 @@ mongoose.connect(URI).then(()=>{
 app.set("view engine","ejs");
 
 // middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(express.json());
+
 
 // use express session to keep track of the user
+app.use(
+    session({
+            secret : "maikyubatau",
+            resave : false,
+            saveUninitialized : false,
+    })
+);
+
+// routes
+app.get("/",function(req,res){
+    if(req.session.isLoggedIn){
+        return res.redirect("/user/dashboard");
+    }else if(req.session.employeeIsLoggedIn){
+        return res.redirect("/employee/dashboard");
+    }
+    res.render("home");
+});
 
 
 
+// server
 app.listen(PORT,function(){
     console.log(`Listening at port ${PORT}`);
 });
